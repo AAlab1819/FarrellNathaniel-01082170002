cbf72cec081299e10291b6d1313b5f690e5e640a
Farrell Nathaniel <42845231+farrellnt@users.noreply.github.com>
2018-09-13 18:54:53 +0700

    Create SpykeTalks-291A.py

---
+++ Week02-Sorting/SpykeTalks-291A.py
@@ -0,0 +1,38 @@
+def cocktail_sort(a):
+    n = len(a)
+    swapped = True
+    start = 0
+    end = n-1
+    while (swapped == True):
+        swapped = False
+        for i in range (start, end):
+            if (a[i] > a[i + 1]) :
+                a[i], a[i + 1]= a[i + 1], a[i]
+                swapped = True
+        if (swapped == False):
+            break
+        swapped = False
+        end = end-1
+        for i in range(end-1, start-1, -1):
+            if (a[i] > a[i + 1]):
+                a[i], a[i + 1] = a[i + 1], a[i]
+                swapped = True
+        start = start + 1
+        
+n = int(input())
+x = list(map(int, input().split()))
+ans = 0
+cocktail_sort(x)
+for i in range(len(x) - 1):
+    if (i + 2) >= n:
+        if (x[i] == x[i + 1]) and (x[i] != 0):
+            ans  = ans + 1
+            i  = i + 1
+    else:
+        if  x[i] == x[i + 1] and x[i] != 0 and x[i] != x[i + 2]:
+            ans = ans + 1
+            i  = i + 1
+        elif x[i] == x[i + 1] and x[i] != 0 and x[i] != 0:
+            ans = -1
+            break
+print(ans)
