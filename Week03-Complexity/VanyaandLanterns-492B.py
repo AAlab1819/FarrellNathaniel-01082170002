e4c2950d386e4dd2ff69611c81d8cf4695980858
Farrell Nathaniel <42845231+farrellnt@users.noreply.github.com>
2018-09-20 22:50:25 +0700

    Update VanyaandLanterns-492B.py

--- Week03-Complexity/VanyaandLanterns-492B.py
+++ Week03-Complexity/VanyaandLanterns-492B.py
@@ -1,6 +1,6 @@
-n, l = map(int,input().split())
-a = sorted(list(map(int,input().split())))
-d = min(a) if min(a) > l - max(a) else l - max(a)
-for i in range(n-1):
-    if (a[i+1] - a[i])/2 > d: d = (a[i+1] - a[i])/2
+n, l = map(int, input().split())
+a = sorted(map(int, input().split()))
+d = max(min(a), l - max(a))
+for i in range (n-1):
+    d = max(d, (a[i+1] - a[i]) / 2)
 print(d)
