8a8d98b517b0abb7f9eefc14b1c28febf63c1e3d
Farrell Nathaniel <42845231+farrellnt@users.noreply.github.com>
2018-10-04 22:14:28 +0700

    Create ChristmasSpruce-913B.cpp

---
+++ Week05-BST/ChristmasSpruce-913B.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int vertices, parent, leaves; bool spruce = true;
+    cin >> vertices;
+    map <int, vector <int> > tree;
+    
+    for(int i = 2; i <= vertices; i++) 
+    {
+        cin >> parent;
+        tree[parent].push_back(i);
+    }
+    
+    for(int i = 1; i <= vertices; i++) 
+    {
+        if(tree[i].size() == 0) continue;
+        else 
+        {
+            leaves = 0;
+            for(int j = 0; j < tree[i].size(); j++) 
+            {
+                if(tree[tree[i][j]].size() == 0)
+                    leaves++;
+            }
+                if(leaves < 3) 
+                    spruce = false;    
+        }
+    }
+        if(spruce)
+        {
+            cout << "Yes" << endl;
+        }
+        else 
+        {
+            cout << "No" << endl;
+        }
+    return 0;
+}
