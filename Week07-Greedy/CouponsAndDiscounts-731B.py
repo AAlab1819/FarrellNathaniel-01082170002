04660394c5f107873d493ba07976c94f403003de
Farrell Nathaniel <42845231+farrellnt@users.noreply.github.com>
2018-10-25 22:15:23 +0700

    Create CouponsAndDiscounts-731B.py

---
+++ Week07-Greedy/CouponsAndDiscounts-731B.py
@@ -0,0 +1,19 @@
+n = int(input())
+teams = [int(x) for x in input().split()]
+carrying = 0
+no_team = False
+
+for i in range(n):
+    if teams[i] == 0 and carrying == 1:
+        print("NO")
+        exit()
+    if teams[i] % 2 == 1:
+        if carrying == 0:
+            carrying = 1
+        else:
+            carrying = 0
+            
+if carrying == 0:
+    print("YES")
+else:
+    print("NO")
