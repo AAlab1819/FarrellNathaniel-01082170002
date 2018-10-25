862222046c92496c4ba5ab7dc938825a79a2b15c
Farrell Nathaniel <42845231+farrellnt@users.noreply.github.com>
2018-10-25 23:12:51 +0700

    Update Partition-946A.py

--- Week07-Greedy/Partition-946A.py
+++ Week07-Greedy/Partition-946A.py
@@ -1,10 +1,10 @@
 n = int(input())
 a = [int(x) for x in input().split()]
-b=0
-c=0
+b = 0
+c = 0
 for i in range(n):
-    if(a[i]>=0):
-        b+=a[i]
+    if(a[i] >= 0):
+        b += a[i]
     else:
-        c+=a[i]
+        c += a[i]
 print(b-c)
